d9f4570df91371dd7f2ad1913b983383b0718eaa
Sean Richards <Krytic@users.noreply.github.com>
2023-04-21 13:00:14 +1200

    add QualityControl script

--- kaitiaki/__init__.py
+++ kaitiaki/__init__.py
@@ -19,7 +19,9 @@ _/j  L l\_!  _//^---^\\_
 Here be wizard.
 """
 
-import pkgutil
+import os
+
+import pkg_resources
 
 import kaitiaki.STARSController as STARS
 import kaitiaki.GridMaker as gridmaker
@@ -29,16 +31,61 @@ import kaitiaki.classifier as classify
 import kaitiaki.model as model
 import kaitiaki.OptionLexer as lexer
 import kaitiaki.kicks as kicks
+import kaitiaki.quality as quality
 
 import kaitiaki.terminal as terminal
 import kaitiaki.file as file
 import kaitiaki.file_handlers as _filehandler
 
+from .utils import transforms
+
 import glisten
 
 from kaitiaki._metadata import __version__
 
-DEBUG_MODE = False
+stars = STARS.STARSController
+
+def load_file(filename):
+    data_path = pkg_resources.resource_filename('kaitiaki', '../backup_data')
+
+    pathname = os.path.join(data_path, '..' + os.sep + 'backup_data')
+    pathname = os.path.normpath(pathname)
+
+    if '..' in filename:
+        raise ValueError("Traversing up the filetree is not permitted.")
+
+    allowed_folders = ['data.bak', 'COtables', 'dat', 'modins']
+
+    for folder in allowed_folders:
+        if filename.startswith(folder):
+            break
+    else:
+        raise ValueError(f"Folder not valid (allowed: {'/'.join(allowed_folders)})")
+
+    if not os.path.exists(os.path.join(pathname, filename)):
+        raise IOError(f"{filename} is not a valid kaitiaki internal file.")
+
+    with open(os.path.join(pathname, filename), 'rb') as f:
+        file = f.read().decode('utf-8')
+
+    return file
+
+def format_metallicity(Z):
+    if isinstance(Z, float):
+        if Z in [1e-4, 1e-5]:
+            return {
+                1e-4: 'zem4',
+                1e-5: 'zem5'
+            }[Z]
+        else:
+            Z = str(Z).split('.')[1]
+            Z = 'z' + Z.ljust(3, '0')
+            return Z
+    if isinstance(Z, str):
+        if Z[0] != 'z':
+            return format_metallicity(float(Z))
+        return Z
+    raise ValueError(f"I don't know what {Z} means.")
 
 log = glisten.log.Logger('~/.kaitiaki-log', line_length_break=72*3)
 
