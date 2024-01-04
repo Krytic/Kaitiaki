cd15050d1078eac0d827b5ed0863b301c052aa38
Sean Richards <Krytic@users.noreply.github.com>
2024-01-04 17:42:27 +1300

    update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,9 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../kaitiaki'))
+sys.path.insert(0, os.path.abspath('../kaitiaki/file_handlers'))
+sys.path.insert(0, os.path.abspath('../kaitiaki/utils'))
 
 
 # -- Project information -----------------------------------------------------
